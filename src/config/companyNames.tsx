c403401d3b24503049d06b105ea2ae7272839f60
prathapjai <vijayprathap.kumar@shoregrp.com>
2024-06-19 19:19:03 +0530

    Logo Images Added

--- src/config/companyNames.tsx
+++ src/config/companyNames.tsx
@@ -1,82 +1,82 @@
 export const companiesNames = [
-    {
-      name: "Tata Consultancy Services",
-      logo: "https://example.com/tcs-logo.png"
-    },
-    {
-      name: "Infosys",
-      logo: "https://example.com/infosys-logo.png"
-    },
-    {
-      name: "Wipro",
-      logo: "https://example.com/wipro-logo.png"
-    },
-    {
-      name: "HCL Technologies",
-      logo: "https://example.com/hcl-logo.png"
-    },
-    {
-      name: "Tech Mahindra",
-      logo: "https://example.com/techm-logo.png"
-    },
-    {
-      name: "Larsen & Toubro Infotech",
-      logo: "https://example.com/lti-logo.png"
-    },
-    {
-      name: "Mindtree",
-      logo: "https://example.com/mindtree-logo.png"
-    },
-    {
-      name: "Mphasis",
-      logo: "https://example.com/mphasis-logo.png"
-    },
-    {
-      name: "Persistent Systems",
-      logo: "https://example.com/persistent-logo.png"
-    },
-    {
-      name: "Hexaware Technologies",
-      logo: "https://example.com/hexaware-logo.png"
-    },
-    {
-      name: "Zensar Technologies",
-      logo: "https://example.com/zensar-logo.png"
-    },
-    {
-      name: "Cyient",
-      logo: "https://example.com/cyient-logo.png"
-    },
-    {
-      name: "NIIT Technologies",
-      logo: "https://example.com/niit-tech-logo.png"
-    },
-    {
-      name: "Oracle Financial Services Software",
-      logo: "https://example.com/ofss-logo.png"
-    },
-    {
-      name: "KPIT Technologies",
-      logo: "https://example.com/kpit-logo.png"
-    },
-    {
-      name: "L&T Technology Services",
-      logo: "https://example.com/ltts-logo.png"
-    },
-    {
-      name: "Mastek",
-      logo: "https://example.com/mastek-logo.png"
-    },
-    {
-      name: "Mindfire Solutions",
-      logo: "https://example.com/mindfire-logo.png"
-    },
-    {
-      name: "Polaris Consulting & Services",
-      logo: "https://example.com/polaris-logo.png"
-    },
-    {
-      name: "QuEST Global",
-      logo: "https://example.com/quest-global-logo.png"
-    }
-  ];
+  {
+    name: "Tata Consultancy Services",
+    logo: "/image/company-logos/TCS-Logo.png",
+  },
+  {
+    name: "Infosys",
+    logo: "/image/company-logos/Infosys_logo.png",
+  },
+  {
+    name: "Wipro",
+    logo: "/image/company-logos/Wipro-logo.png",
+  },
+  {
+    name: "HCL Technologies",
+    logo: "/image/company-logos/HCL-Logo.png",
+  },
+  {
+    name: "Tech Mahindra",
+    logo: "/image/company-logos/Tech_Mahindra_New_Logo.png",
+  },
+  {
+    name: "Larsen & Toubro Infotech",
+    logo: "/image/company-logos/LandT_Infotech.png",
+  },
+  {
+    name: "Mindtree",
+    logo: "/image/company-logos/Mind-Tree-Logo.png",
+  },
+  {
+    name: "Mphasis",
+    logo: "/image/company-logos/Mphasis-logo.png",
+  },
+  {
+    name: "Persistent Systems",
+    logo: "/image/company-logos/Persistent-logo.png",
+  },
+  {
+    name: "Hexaware Technologies",
+    logo: "/image/company-logos/HexAware-logo.png",
+  },
+  {
+    name: "Zensar Technologies",
+    logo: "/image/company-logos/ZenSar-logo.png",
+  },
+  {
+    name: "Cyient",
+    logo: "/image/company-logos/Cyient-logo.png",
+  },
+  {
+    name: "NIIT Technologies",
+    logo: "/image/company-logos/NIIT_Technologies_Logo.png",
+  },
+  {
+    name: "Oracle Financial Services Software",
+    logo: "/image/company-logos/Oracle_logo.png",
+  },
+  {
+    name: "KPIT Technologies",
+    logo: "/image/company-logos/KPIT-TECH-logo.png",
+  },
+  {
+    name: "L&T Technology Services",
+    logo: "/image/company-logos/L&T-Logo.png",
+  },
+  {
+    name: "Mastek",
+    logo: "/image/company-logos/Mastak-logo.png",
+  },
+  {
+    name: "Mindfire Solutions",
+    logo: "/image/company-logos/Mindfire-logo.png",
+  },
+  {
+    name: "Polaris Consulting & Services",
+    logo: "/image/company-logos/Polaris-logo.png",
+  },
+  {
+    name: "QuEST Global",
+    logo: "/image/company-logos/Quest-logo.png",
+  },
+];
--- src/page-component/companyCards/CompanyCards.tsx
+++ src/page-component/companyCards/CompanyCards.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { Fragment } from "react";
 import {
   CompanyCardsWrapper,
   CompanyCard,
   TitleStyled,
   SubTitleStyled,
+  CompanyNameStyled,
+  ImageStyled,
 } from "./CompanyCards.styled";
 import { companiesNames } from "@/config/companyNames";
 
@@ -24,7 +27,10 @@ const CompanyReviewComponent = () => {
       {renderTitle()}
       <CompanyCardsWrapper className="layout-gap">
         {companiesNames.map((companyName, index) => (
-          <CompanyCard key={index}>{companyName.name}</CompanyCard>
+          <CompanyCard key={index}>
+            <ImageStyled src={companyName.logo} alt="No logo" />
+            <CompanyNameStyled>{companyName.name}</CompanyNameStyled>
+          </CompanyCard>
         ))}
       </CompanyCardsWrapper>
     </Fragment>
